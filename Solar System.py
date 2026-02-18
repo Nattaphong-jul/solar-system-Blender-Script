303380adaf77b77c15cebe600983f15a1dcb22a0
Nattaphong-jul <nattapong.jullaya@gmail.com>
2026-02-18 17:28:14 +0700

    Edit HDRI part

--- Solar System.py
+++ Solar System.py
@@ -43,7 +43,7 @@ def create_sphere(name, location, scale):
     return sphere
 
 
-def apply_img_texture(target_obj, mat_name, img_path, metallic_val=0.0, roughness_val=0.5, emit_strength=1.0):
+def apply_img_texture(target_obj, mat_name, img_path, metallic=0.0, roughness=0.5, emit_strength=1.0):
     
     # Create New Material
     myMat = bpy.data.materials.new(name=str(mat_name))
@@ -78,88 +78,49 @@ def apply_img_texture(target_obj, mat_name, img_path, metallic_val=0.0, roughnes
     links.new(texture_node.outputs['Color'], principled_node.inputs['Emission Color'])
     
     # BSDF Values Setting
-    principled_node.inputs['Metallic'].default_value = metallic_val
-    principled_node.inputs['Roughness'].default_value = roughness_val
+    principled_node.inputs['Metallic'].default_value = metallic
+    principled_node.inputs['Roughness'].default_value = roughness
     principled_node.inputs['Emission Strength'].default_value = emit_strength
     
     return myMat
 
-
 def jupiter():
     planet = create_sphere("Jupiter", (110,0,0), (139.8))
     
-    apply_img_texture(target_obj=planet, mat_name="Jupiter", img_path="//texture\\jupiter\\2k_jupiter.jpg")
-
+    apply_img_texture(target_obj=planet, mat_name="Jupiter", img_path="//texture\\jupiter\\2k_jupiter.jpg", emit_strength=1)
 
 jupiter()
 
 
-def apply_hdri(hdri_path):
-    """
-    Applies an HDRI image to the current scene's world background.
-    """
-    # 1. Get the current world (or create one if none exists)
+def apply_hdri(img_path):
+    # Get the current world background
     world = bpy.context.scene.world
-    if not world:
-        world = bpy.data.worlds.new("World")
-        bpy.context.scene.world = world
-    
-    # 2. Enable 'Use Nodes'
     world.use_nodes = True
-    node_tree = world.node_tree
-    nodes = node_tree.nodes
-    links = node_tree.links
+    nodes = world.node_tree.nodes
+    links = world.node_tree.links
     
-    # 3. Clear existing nodes to start fresh
-    nodes.clear()
+    # Clear nodes
+    for node in nodes:
+        nodes.remove(node)
     
-    # 4. Create the necessary nodes
-    # Texture Coordinate (for mapping, useful if you want to rotate later)
-    tex_coord_node = nodes.new(type='ShaderNodeTexCoord')
-    tex_coord_node.location = (-600, 0)
-    
-    # Mapping (to rotate the HDRI)
-    mapping_node = nodes.new(type='ShaderNodeMapping')
-    mapping_node.location = (-400, 0)
-    
-    # Environment Texture (This holds the HDRI image)
-    env_tex_node = nodes.new(type='ShaderNodeTexEnvironment')
-    env_tex_node.location = (-200, 0)
-    
-    # Background Node
-    bg_node = nodes.new(type='ShaderNodeBackground')
-    bg_node.location = (0, 0)
-    
-    # World Output Node
+    # Important Texture Node
     output_node = nodes.new(type='ShaderNodeOutputWorld')
-    output_node.location = (200, 0)
+    bg_node = nodes.new(type='ShaderNodeBackground')
+    env_tex_node = nodes.new(type='ShaderNodeTexEnvironment')
     
-    # 5. Load the HDRI Image
-    if os.path.exists(hdri_path):
-        try:
-            img = bpy.data.images.load(hdri_path)
-            env_tex_node.image = img
-        except:
-            print(f"Could not load image at {hdri_path}")
-    else:
-        print(f"File not found: {hdri_path}")
-        return
+    # Set Nodes Location
+    bg_node.location = (0, 0)
+    env_tex_node.location = (-300, 0)
+    
+    # Image Texture
+    abs_img_path = bpy.path.abspath(img_path)
+    loaded_image = bpy.data.images.load(abs_img_path)
+    env_tex_node.image = loaded_image
+    
+    # Node Link
+    links.new(env_tex_node.outputs['Color'], bg_node.inputs['Color'])
+    links.new(bg_node.outputs['Background'], output_node.inputs['Surface'])
+    
+    return world
 
-    # 6. Link the nodes together
-    # TexCoord -> Mapping
-    links.new(tex_coord_node.outputs["Generated"], mapping_node.inputs["Vector"])
-    # Mapping -> Environment Texture
-    links.new(mapping_node.outputs["Vector"], env_tex_node.inputs["Vector"])
-    # Environment Texture -> Background
-    links.new(env_tex_node.outputs["Color"], bg_node.inputs["Color"])
-    # Background -> Output
-    links.new(bg_node.outputs["Background"], output_node.inputs["Surface"])
-
-    print("HDRI Setup Complete.")
-
-# --- USAGE ---
-# Replace this path with the actual path to your .exr or .hdr file
-# Note: On Windows, use r"Path" or double backslashes \\
-my_hdri_path = r"S:\Google Drive\3D\Solar System\texture\HDRI\8k_stars_milky_way.jpg"
-
-apply_hdri(my_hdri_path)
+apply_hdri("//texture\\HDRI\\8k_stars_milky_way.jpg")
